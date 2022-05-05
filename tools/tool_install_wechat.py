ba33ba80501d58f9333be11004403132a5e67e94
fishros <sangxin2014@sina.com>
2022-05-05 13:18:47 +0800

    [feat]:添加桌面版微信支持

--- tools/tool_install_wechat.py
+++ tools/tool_install_wechat.py
@@ -24,40 +24,60 @@ newgrp docker
 """.format(name,name,name,name,name,name)
 
     def install_wechat(self):
-        run_tool_file('tools.tool_install_docker')
-        user =  FileUtils.getusers()[0]
-        name = 'wechat'
-        inputs = 'fcitx'
-        bashrc = '/home/{}/.bashrc'.format(user)
-        bin_path = "/home/{}/.fishros/bin/".format(user)
+        """
+        # 桌面版微信
+        git clone https://gitee.com/ohhuo/wechat_deb.git /tmp/wechaet_deb
+        cd /tmp/wechaet_deb && cat wechat_* > wechat.deb 
+        cd /tmp/wechaet_deb && sudo dpkg -i wechat.deb
+        rm -rf /tmp/wechaet_deb 
+        # https://fishros.org.cn/forum/topic/195
+        # sudo apt remove wechat-linux-spark
 
-        version_dic = {1:"ibus(系统默认)",2:"fcitx"}
-        code,_ = ChooseTask(version_dic,"请选择系统输入法版本(如果不知道怎么选,请选1-ibus):",False).run()
-        if code == 1: inputs='ibus'
-        file_path = '/home/{}/docker/{}/WeChatFiles'.format(user,name)
-        CmdTask('mkdir -p {}'.format(file_path),os_command=True).run()
-        PrintUtils.print_info("正在为您拉取微信镜像")
-        CmdTask('sudo docker pull bestwu/wechat ',os_command=True).run()
-        CmdTask('sudo docker pull bestwu/wechat ',os_command=True).run()
-        PrintUtils.print_info("正在为创建安装微信")
-        result = CmdTask('sudo docker run -d --name wechat --device /dev/snd --ipc="host"  -v /tmp/.X11-unix:/tmp/.X11-unix  \
-        -v {}:/WeChatFiles  -e DISPLAY=unix$DISPLAY  -e XMODIFIERS=@im={}  -e QT_IM_MODULE={}  -e GTK_IM_MODULE={}  -e AUDIO_GID=`getent group audio | cut -d: -f3` bestwu/wechat'.format(file_path,inputs,inputs,inputs),
-        os_command=False).run()
+        """
+        wechat_version_dic = {1:"Docker版本",2:"桌面版本"}
+        code,_ = ChooseTask(wechat_version_dic,"请选择微信版本(两个版本区别对比:https://fishros.org.cn/forum/topic/195):",False).run()
+        if code==2:
+            AptUtils.install_pkg("git")
+            CmdTask('sudo apt install git',os_command=True).run()
+            CmdTask('git clone https://gitee.com/ohhuo/wechat_deb.git /tmp/wechaet_deb',os_command=True).run()
+            CmdTask('cd /tmp/wechaet_deb && cat wechat_* > wechat.deb',os_command=True).run()
+            CmdTask('cd /tmp/wechaet_deb && sudo dpkg -i wechat.deb',os_command=True).run()
+            CmdTask('rm -rf /tmp/wechaet_deb',os_command=True).run()
+            PrintUtils.print_success("以为你安装完成wechat~")
+        if code == 1:
+            run_tool_file('tools.tool_install_docker')
+            user =  FileUtils.getusers()[0]
+            name = 'wechat'
+            inputs = 'fcitx'
+            bashrc = '/home/{}/.bashrc'.format(user)
+            bin_path = "/home/{}/.fishros/bin/".format(user)
 
-        if result[0] != 0: PrintUtils.print_error("失败了。。请到社区或群聊反馈:{}".format(result[1]+result[2]))
-        PrintUtils.print_info("===========================================")
-        PrintUtils.print_success("以为你安装完成wechat，稍后会弹窗登录")
-        # 创建微信脚本
-        FileUtils.new(bin_path,name,self.get_wechat_scripts(name))
-        FileUtils.find_replace_sub(bashrc,"# >>> fishros scripts >>>","# <<< fishros scripts <<<", "")
-        FileUtils.append(bashrc,'# >>> fishros scripts >>>\nexport PATH=$PATH:{} \n# <<< fishros scripts <<<'.format(bin_path))
-        CmdTask('sudo chmod 777 {}'.format(bin_path+name),os_command=True).run()
+            version_dic = {1:"ibus(系统默认)",2:"fcitx"}
+            code,_ = ChooseTask(version_dic,"请选择系统输入法版本(如果不知道怎么选,请选1-ibus):",False).run()
+            if code == 1: inputs='ibus'
+            file_path = '/home/{}/docker/{}/WeChatFiles'.format(user,name)
+            CmdTask('mkdir -p {}'.format(file_path),os_command=True).run()
+            PrintUtils.print_info("正在为您拉取微信镜像")
+            CmdTask('sudo docker pull bestwu/wechat ',os_command=True).run()
+            CmdTask('sudo docker pull bestwu/wechat ',os_command=True).run()
+            PrintUtils.print_info("正在为创建安装微信")
+            result = CmdTask('sudo docker run -d --name wechat --device /dev/snd --ipc="host"  -v /tmp/.X11-unix:/tmp/.X11-unix  \
+            -v {}:/WeChatFiles  -e DISPLAY=unix$DISPLAY  -e XMODIFIERS=@im={}  -e QT_IM_MODULE={}  -e GTK_IM_MODULE={}  -e AUDIO_GID=`getent group audio | cut -d: -f3` bestwu/wechat'.format(file_path,inputs,inputs,inputs),
+            os_command=False).run()
 
-        PrintUtils.print_info("=================后续使用指令=================")
-        PrintUtils.print_warn("后续可在任意终端输入{}来启动/停止微信".format(name))
-        PrintUtils.print_info("=================文件存储位置================")
-        PrintUtils.print_warn("微信所有文件已经放到目录：{}".format(file_path))
-        # CmdTask('',os_command=True).run()
+            if result[0] != 0: PrintUtils.print_error("失败了。。请到社区或群聊反馈:{}".format(result[1]+result[2]))
+            PrintUtils.print_info("===========================================")
+            PrintUtils.print_success("以为你安装完成wechat，稍后会弹窗登录")
+            # 创建微信脚本
+            FileUtils.new(bin_path,name,self.get_wechat_scripts(name))
+            FileUtils.find_replace_sub(bashrc,"# >>> fishros scripts >>>","# <<< fishros scripts <<<", "")
+            FileUtils.append(bashrc,'# >>> fishros scripts >>>\nexport PATH=$PATH:{} \n# <<< fishros scripts <<<'.format(bin_path))
+            CmdTask('sudo chmod 777 {}'.format(bin_path+name),os_command=True).run()
+
+            PrintUtils.print_info("=================后续使用指令=================")
+            PrintUtils.print_warn("后续可在任意终端输入{}来启动/停止微信".format(name))
+            PrintUtils.print_info("=================文件存储位置================")
+            PrintUtils.print_warn("微信所有文件已经放到目录：{}".format(file_path))
 
 
     def run(self):
