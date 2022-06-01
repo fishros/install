7e06b3498eb9d2816243c7e9585b97c8af9507ba
鱼香ROS <87068644+fishros@users.noreply.github.com>
2022-06-01 18:42:09 +0800

    [tmp]:临时修改去除ghproxy

--- tools/tool_install_code_server.py
+++ tools/tool_install_code_server.py
@@ -14,9 +14,9 @@ class Tool(BaseTool):
         PrintUtils.print_info("开始根据系统架构,为你下载对应版本的Code-Server~")
         # 根据系统架构下载不同版本的安装包
         if osarch=='amd64':
-            CmdTask('sudo wget https://ghproxy.com/https://github.com/coder/code-server/releases/download/v4.4.0/code-server_4.4.0_amd64.deb -O /tmp/code-server.deb',os_command=True).run()
+            CmdTask('sudo wget https://github.com/coder/code-server/releases/download/v4.4.0/code-server_4.4.0_amd64.deb -O /tmp/code-server.deb',os_command=True).run()
         elif osarch=='arm64':
-            CmdTask('sudo wget https://ghproxy.com/https://github.com/coder/code-server/releases/download/v4.4.0/code-server_4.4.0_arm64.deb -O /tmp/code-server.deb',os_command=True).run()
+            CmdTask('sudo wget https://github.com/coder/code-server/releases/download/v4.4.0/code-server_4.4.0_arm64.deb -O /tmp/code-server.deb',os_command=True).run()
         else:
             return False
         PrintUtils.print_info("下载完成,接下来为你安装Code-Server~")
