dff3e393fff9b8a127e649bb94be0ea00bfb6780
鱼香ROS <87068644+fishros@users.noreply.github.com>
2022-08-15 13:13:05 +0800

    feat(vscode): 更换vscode到最新版本

--- tools/tool_install_vscode.py
+++ tools/tool_install_vscode.py
@@ -14,9 +14,9 @@ class Tool(BaseTool):
         PrintUtils.print_info("开始根据系统架构,为你下载对应版本的vscode~")
         # 根据系统架构下载不同版本的安装包
         if osarch=='amd64':
-            CmdTask('sudo wget http://vscode.cdn.azure.cn/stable/5554b12acf27056905806867f251c859323ff7e9/code_1.64.0-1643863948_amd64.deb -O /tmp/vscode.deb',os_command=True).run()
+            CmdTask('sudo wget http://vscode.cdn.azure.cn/stable/6d9b74a70ca9c7733b29f0456fd8195364076dda/code_1.70.1-1660113095_amd64.deb -O /tmp/vscode.deb',os_command=True).run()
         elif osarch=='arm64':
-            CmdTask('sudo wget http://vscode.cdn.azure.cn/stable/5554b12acf27056905806867f251c859323ff7e9/code_1.64.0-1643862176_arm64.deb -O /tmp/vscode.deb',os_command=True).run()
+            CmdTask('sudo wget http://vscode.cdn.azure.cn/stable/6d9b74a70ca9c7733b29f0456fd8195364076dda/code_1.70.1-1660113095_arm64.deb -O /tmp/vscode.deb',os_command=True).run()
         else:
             return False
         PrintUtils.print_info("下载完成,接下来为你安装Vscode~")
