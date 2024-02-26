d24877a54f0defb31c355ddc32ea2a683636b417
鱼香ROS <87068644+fishros@users.noreply.github.com>
2024-02-26 21:16:53 +0800

    fix: update vscode addr

--- tools/tool_install_vscode.py
+++ tools/tool_install_vscode.py
@@ -17,9 +17,9 @@ class Tool(BaseTool):
         PrintUtils.print_info("开始根据系统架构,为你下载对应版本的vscode~")
         # 根据系统架构下载不同版本的安装包
         if osarch=='amd64':
-            CmdTask('sudo wget http://vscode.cdn.azure.cn/stable/abd2f3db4bdb28f9e95536dfa84d8479f1eb312d/code_1.82.2-1694671812_amd64.deb -O /tmp/vscode.deb',os_command=True).run()
+            CmdTask('sudo wget https://vscode.download.prss.microsoft.com/dbazure/download/stable/903b1e9d8990623e3d7da1df3d33db3e42d80eda/code_1.86.2-1707854558_amd64.deb -O /tmp/vscode.deb',os_command=True).run()
         elif osarch=='arm64':
-            CmdTask('sudo wget http://vscode.cdn.azure.cn/stable/abd2f3db4bdb28f9e95536dfa84d8479f1eb312d/code_1.82.2-1694671436_arm64.deb -O /tmp/vscode.deb',os_command=True).run()
+            CmdTask('sudo wget https://vscode.download.prss.microsoft.com/dbazure/download/stable/903b1e9d8990623e3d7da1df3d33db3e42d80eda/code_1.86.2-1707853305_arm64.deb -O /tmp/vscode.deb',os_command=True).run()
         else:
             return False
         PrintUtils.print_info("下载完成,接下来为你安装Vscode~")
