fc287d1371c162b021e7c488b5b905f82e262be7
鱼香ROS <87068644+fishros@users.noreply.github.com>
2024-09-04 17:29:05 +0800

    fix(trans): 修复多语言支持url错误

--- tools/translation/translator.py
+++ tools/translation/translator.py
@@ -10,7 +10,7 @@ import tools.base
 from tools.base import CmdTask
 
 _suported_languages = ['zh_CN', 'en_US']
-url_prefix = os.environ.get('FISHROS_URL')
+url_prefix = os.environ.get('FISHROS_URL')+'/'
 lang_url = url_prefix+'tools/translation/assets/{}.py'
 
 
