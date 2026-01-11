cb935b6927db138313e7befca52255cdb1eeac28
FishROS <87068644+fishros@users.noreply.github.com>
2026-01-11 15:08:16 +0800

    fix: 针对环境变量获取异常问题进行修复

--- install.py
+++ install.py
@@ -2,7 +2,7 @@
 import os
 import importlib
 
-url_prefix = os.environ.get('FISHROS_URL','')
+url_prefix = os.environ.get('FISHROS_URL','http://mirror.fishros.com/install/')
 base_url = os.path.join(url_prefix,'tools/base.py')
 translator_url = os.path.join(url_prefix,'tools/translation/translator.py')
 
@@ -54,7 +54,7 @@ for tool_id, tool_info in tools.items():
 tracking = None
 def main():
     os.system("mkdir -p /tmp/fishinstall/tools/translation/assets")
-    url_prefix = os.environ.get('FISHROS_URL','')
+    url_prefix = os.environ.get('FISHROS_URL','http://mirror.fishros.com/install/')
     if url_prefix:
         os.system("wget {} -O /tmp/fishinstall/{} --no-check-certificate".format(base_url,base_url.replace(url_prefix,'')))
         os.system("wget {} -O /tmp/fishinstall/{} --no-check-certificate".format(translator_url,translator_url.replace(url_prefix,'')))
